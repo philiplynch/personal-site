40e3bb93363a6c3de86b76c9865a341e83864b93
philiplynch <107075994+philiplynch@users.noreply.github.com>
2023-02-19 16:19:29 -0330

    Update data.js

--- menu/data.js
+++ menu/data.js
@@ -27,4 +27,15 @@ export const projects = [
         
     }
     
+    {
+        name: "Guestbook",
+        url: "guestbook/index.html",
+        completed_on: "February 15, 2023",
+        description: "A guestbook which allows users to specify their name, location, and comment, which will then be added to the list automatically upon clicking Submit",
+        jsconcepts: [
+            "ES6 Modules", "import statement", "export keyword", "document.getElementById()", "addEventListener()", "innerHTML", "Template literals", "Array.map()", "Array.join()", "Object properties", "Array.unshift()", "Function definition", "Error handling", "External library"
+  ]
+        
+    }
+    
 ]
